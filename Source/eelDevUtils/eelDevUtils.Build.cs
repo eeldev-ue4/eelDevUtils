8b45539494af0682561b8420b8fd9ca97176bd08
hulken <hulkas@gmail.com>
2022-03-25 09:41:04 +0100

    Update eelDevUtils.Build.cs

--- Source/eelDevUtils/eelDevUtils.Build.cs
+++ Source/eelDevUtils/eelDevUtils.Build.cs
@@ -8,47 +8,22 @@ public class eelDevUtils : ModuleRules
 	{
 		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
 		
-		PublicIncludePaths.AddRange(
-			new string[] {
-				// ... add public include paths required here ...
-			}
-			);
-				
+		PublicDependencyModuleNames.AddRange(new string[]
+		{
+			"Core",
+			"Engine",
+		});
 		
-		PrivateIncludePaths.AddRange(
-			new string[] {
-				// ... add other private include paths required here ...
-			}
-			);
-			
-		
-		PublicDependencyModuleNames.AddRange(
-			new string[]
-			{
-				"Core",
-				// ... add other public dependencies that you statically link with here ...
-			}
-			);
-			
-		
-		PrivateDependencyModuleNames.AddRange(
-			new string[]
-			{
-				"CoreUObject",
-				"Engine",
-				"Slate",
-				"SlateCore",
-				// ... add private dependencies that you statically link with here ...	
-			}
-			);
-		
-		
-		DynamicallyLoadedModuleNames.AddRange(
-			new string[]
-			{
-				// ... add any modules that your module loads dynamically here ...
-			}
-			);
+		PrivateDependencyModuleNames.AddRange(new string[]
+		{
+			"CoreUObject",
+			"Slate",
+			"SlateCore",
+			"RHI",
+			"Renderer",
+			"RenderCore",
+			"Projects",
+		});
 		
 		if (Target.Platform == UnrealTargetPlatform.Mac)
 		{
